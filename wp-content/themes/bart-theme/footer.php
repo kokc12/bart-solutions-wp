71238494dffbb729e9a3d0bfca31e902b55c05b2
Yurii Koshyk <y.koshyk12@gmail.com>
2023-11-23 03:13:55 +0200

    edit footer links / remove standart theme readme file / edit info in package.json

--- wp-content/themes/bart-theme/footer.php
+++ wp-content/themes/bart-theme/footer.php
@@ -13,17 +13,12 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bart-theme' ) ); ?>">
+			<a href="<?php echo esc_url( __( 'https://www.bart-solutions.com/', 'bart-theme' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'bart-theme' ), 'WordPress' );
+				printf( esc_html__( 'Proudly powered by %s', 'bart-theme' ), 'bART-Solutions' );
 				?>
 			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'bart-theme' ), 'bart-theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
